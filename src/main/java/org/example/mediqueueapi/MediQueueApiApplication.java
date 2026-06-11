714e9123497457f7e56db6859292085da1f4c868
ghusharib najam <fa23-bcs-054@cuilahore.edu.pk>
2026-06-11 15:56:55 +0500

    Initial commit

---
+++ src/main/java/org/example/mediqueueapi/MediQueueApiApplication.java
@@ -0,0 +1,13 @@
+package org.example.mediqueueapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediQueueApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MediQueueApiApplication.class, args);
+    }
+
+}
